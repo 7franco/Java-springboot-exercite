fa600c53216013442b58d85ad532c270d3b1988c
Jonathan Franco <jcrfranco7@gmail.com>
2025-09-24 23:24:23 -0500

    Añadiendo el filtro JwtAuthenticationFilter para el login

--- springboot-crud/src/main/java/com/franco/repasando/springboot/app/springboot_crud/entities/User.java
+++ springboot-crud/src/main/java/com/franco/repasando/springboot/app/springboot_crud/entities/User.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.franco.repasando.springboot.app.springboot_crud.validation.ExistsByUsername;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -29,9 +30,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
-    @Column(unique = true)
+    //@ExistsByUsername
     @NotBlank
     @Size(min = 4, max = 12)
+    @Column(unique = true)
     private String username;
 
     @NotBlank
