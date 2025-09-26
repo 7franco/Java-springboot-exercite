425c95f565ce43b94721e4362130f5c7fb72cbab
Jonathan Franco <jcrfranco7@gmail.com>
2025-09-25 23:19:04 -0500

    Configurando Cors para los clientes

--- springboot-crud/src/main/java/com/franco/repasando/springboot/app/springboot_crud/controllers/ProductController.java
+++ springboot-crud/src/main/java/com/franco/repasando/springboot/app/springboot_crud/controllers/ProductController.java
@@ -18,6 +18,7 @@ import com.franco.repasando.springboot.app.springboot_crud.services.ProductServi
 
 import jakarta.validation.Valid;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,7 +27,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 
-
+@CrossOrigin(origins = "https://localhost:4200", originPatterns = "*")
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
