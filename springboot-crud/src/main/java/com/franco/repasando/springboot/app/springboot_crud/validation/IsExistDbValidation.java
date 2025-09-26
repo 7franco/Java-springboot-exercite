28670e37e2c7c47a0bcf916593e441ec77ddb528
Jonathan Franco <jcrfranco7@gmail.com>
2025-09-25 22:31:29 -0500

    Añadiendo reglas de seguridad en la Configuración de Spring Security

--- springboot-crud/src/main/java/com/franco/repasando/springboot/app/springboot_crud/validation/IsExistDbValidation.java
+++ springboot-crud/src/main/java/com/franco/repasando/springboot/app/springboot_crud/validation/IsExistDbValidation.java
@@ -16,6 +16,9 @@ public class IsExistDbValidation implements ConstraintValidator<IsExistDb, Strin
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (service ==null) {
+            return true;
+        }
         return !service.existsBySku(value);
     }
 
