7b7aae09ef27a406bfec79029d09eeb202f68d47
Jonathan Franco <jcrfranco7@gmail.com>
2025-09-25 23:02:19 -0500

    Anotaciones de reglas de seguridad de Spring Security (permisos con anotaciones @PreAuthorize("hasRole('ADMIN')"))

--- springboot-crud/src/main/java/com/franco/repasando/springboot/app/springboot_crud/security/SecurityConfig.java
+++ springboot-crud/src/main/java/com/franco/repasando/springboot/app/springboot_crud/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,6 +17,7 @@ import com.franco.repasando.springboot.app.springboot_crud.security.filter.JwtAu
 import com.franco.repasando.springboot.app.springboot_crud.security.filter.JwtValidationFilter;
 
 @Configuration
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
     @Autowired
@@ -31,16 +33,30 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    // @Bean
+    // SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    //     return http.authorizeHttpRequests((authz) -> authz
+    //     .requestMatchers(HttpMethod.GET,"/api/users").permitAll()
+    //     .requestMatchers(HttpMethod.POST,"/api/users/register").permitAll()
+    //     .requestMatchers(HttpMethod.POST,"/api/users").hasRole("ADMIN")
+    //     .requestMatchers(HttpMethod.GET,"/api/products","/api/products/{id}").hasAnyRole("ADMIN","USER")
+    //     .requestMatchers(HttpMethod.POST,"/api/products").hasRole("ADMIN")
+    //     .requestMatchers(HttpMethod.PUT,"/api/products/{id}").hasRole("ADMIN")
+    //     .requestMatchers(HttpMethod.DELETE,"/api/products/{id}").hasRole("ADMIN")
+    //     .anyRequest().authenticated())
+    //     .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+    //     .addFilter(new JwtValidationFilter(authenticationManager()))
+    //     .csrf(config-> config.disable())
+    //     .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).build()
+    //     ;
+    // }
+
+    //con anotacion en los controller @PreAuthorize
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         return http.authorizeHttpRequests((authz) -> authz
         .requestMatchers(HttpMethod.GET,"/api/users").permitAll()
         .requestMatchers(HttpMethod.POST,"/api/users/register").permitAll()
-        .requestMatchers(HttpMethod.POST,"/api/users").hasRole("ADMIN")
-        .requestMatchers(HttpMethod.GET,"/api/products","/api/products/{id}").hasAnyRole("ADMIN","USER")
-        .requestMatchers(HttpMethod.POST,"/api/products").hasRole("ADMIN")
-        .requestMatchers(HttpMethod.PUT,"/api/products/{id}").hasRole("ADMIN")
-        .requestMatchers(HttpMethod.DELETE,"/api/products/{id}").hasRole("ADMIN")
         .anyRequest().authenticated())
         .addFilter(new JwtAuthenticationFilter(authenticationManager()))
         .addFilter(new JwtValidationFilter(authenticationManager()))
