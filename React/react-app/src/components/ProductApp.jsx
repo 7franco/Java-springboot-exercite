05954c768d95d267d12f3de1dbba72bbb2086925
Jonathan Franco <jcrfranco7@gmail.com>
2025-10-02 22:04:59 -0500

    React 2

--- React/react-app/src/components/ProductApp.jsx
+++ React/react-app/src/components/ProductApp.jsx
@@ -38,7 +38,7 @@ export const ProductApp = ({title}) => {
 
     const handlerRemoveProduct = (id) =>{
         console.log(id);
-        setProducts(products.filter(user => user.id != id));
+        setProducts(products.filter(product => product.id != id));
     }
 
     const handlesProductSelected = (product) =>{
